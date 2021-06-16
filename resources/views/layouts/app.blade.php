0997c3ae5e8cf92a5c421ed6c9bc92f3c4c7db89
Elvis <e.igiebor@cyberticks.com>
2021-06-16 12:40:37 +0100

    created Pages and Posts controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Elvis Blog') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,10 +21,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar text-white navbar-expand-md bg-dark  shadow-sm" >
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand text-white" href="{{ url('/') }}">
+                    {{ config('app.name', 'Elvis Blog') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,18 +37,25 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto text-white">
                         <!-- Authentication Links -->
+                        <li class="nav-item">
+                                    <a class="nav-link text-white-50" href="/">Home</a>
+                                    
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white-50" href="/blog">Blog</a>
+                                </li>
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link text-white-50" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link text-white-50" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -78,6 +85,9 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <div>
+           @include('layouts.footer')
+        </div>
     </div>
 </body>
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+<footer class="bg-dark" style="padding-top:20px">
+
+    <div class="container" style="margin-top:20px">
+    <div class="row">
+    <div class="col-md-4">
+    <h6 class="text-white">Pages</h6>
+    <li class="nav-item" style="margin-top:-20px">
+    <a class="nav text-white-50" href="/">Home</a>
+    <a class="nav text-white-50" href="/blog">Blog</a>
+    <a class="nav text-white-50" href="/login">Login</a>
+    <a class="nav text-white-50" href="/register">Register</a>
+    </li>
+    </div>
+
+    <div class="col-md-4">
+    <h6 class="text-white">Find Us</h6>
+    <li class="nav-item" style="margin-top:-20px">
+    <a class="nav text-white-50" href="/">What we do</a>
+    <a class="nav text-white-50" href="/blog">Address</a>
+    <a class="nav text-white-50" href="/login">Phone</a>
+    <a class="nav text-white-50" href="/register">Contact</a>
+    </li>
+    </div>
+
+    <div class="col-md-4">
+    <h6 class="text-white">Latest Posts</h6>
+    <li class="nav-item" style="margin-top:-20px">
+    <a class="nav text-white-50" href="/">Why we love tech</a>
+    <a class="nav text-white-50" href="/blog">Why we love design</a>
+    <a class="nav text-white-50" href="/login">Why we love laravel</a>
+    <a class="nav text-white-50" href="/register">Why we love php</a>
+    </li>
+    </div>
+  
+    </div><br>
+    <hr>
+    <small class="text-white-50">Copywrite 2021 code with Elvis</small>
+    <br><br>
+    </div>
+
+
+</footer>
