7a8c874f53ed262fc21229df87d3169ee5af7ba9
Elvis <e.igiebor@cyberticks.com>
2021-06-16 18:44:30 +0100

    designed the home page

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+ @section('content')
+   <div class="container">
+        <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Blog Posts</h1>
+        </div>
+        </div>
+        <hr>
+   </div>
+   
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
+ <div class="" style="margin-top:-30px">
 
-    <div class="background-image">
-        
+     <div class="">
+       <div class="background-image text-center">
+        <h1 class="text-center text-white" style="padding-top:200px; font-weight:bold; font-size:50px; ">Do you want to be a developer?</h1>
+        <button type="button" class="btn btn-light" style="font-weight:bold;">Read More</button>
+      </div>
+    </div>
+</div><br>
+
+<div class="container">
+    <div class="row">
+    <div class="col-md-6">
+     <img src="https://images.pexels.com/photos/6177661/pexels-photo-6177661.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+     </div>
+
+     <div class="col-md-6">
+        <div class="" style="text-align:center; margin:50px">
+        <h4 class="fw-bolder" style="text-align:left; font-weight:bold">Struggling to be a better web developer?</h4>
+        <p style="text-align:left;">Then you are in the right place</p>
+
+        <div class="">
+        <p  class="" style="text-align:left; font-weight:bold">Explore our site for amazing contents and stop Struggling as a developer</p>
+        <p style="text-align:left"><a class="btn btn-primary br-rounded" style="font-weight:bold; border-radius:20px" href="/blog">Find Out More</a></p>
+        </div>
+        </div>
+     </div>
     </div>
 
+    
+</div><br>
+<div class="text-center bg-dark text-white " style="padding:40px">
+    <h4 class="text-white-">
+     I am an expert in ...
+    </h4>
+    <h3>UX Design<h3>
+    <h3>Project Management<h3>
+    <h3>Digital Strategy<h3>
+    <h3>Backend Development<h3>
+    </div>
+
+    <div class="text-center py-5">
+       <span class="uppercase text-s text-gray-400">
+       BLOG
+       </span>
+       <h2 >Recent Posts</h2><br>
+       <p>My blog posts will stay here. Thanks. My blog posts will stay here. Thanks. My blog posts will stay here. Thanks</p>
+    <div>
+
+    <div class="container">
+        <div class="row bg-danger p-5">
+        <div class="col-md-6 py-5">
+        <h6 class="text-white">PHP</h6>
+        <p class="text-white" style="font-weight:bolder">I like coding php. Its the language for my backend. Just learnt Laravel about a month ago. Its a framework for php. Its makes php easier and faster to code. Yea!</p>
+        <button class="btn btn-transparent border-white text-white" style="border-radius:30px">FIND OUT MORE</button>
+        </div>
+        <div class="col-md-6"> <img src="https://images.pexels.com/photos/6177661/pexels-photo-6177661.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></div>
+        <div>
+    </div>
 @endsection
+
+
+
+
+
+
+
